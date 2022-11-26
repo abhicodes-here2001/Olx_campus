bfbdddcbca6deefc20754bd02f83826417ae71ab
abhicodes-here2001 <abhisargautam01@gmail.com>
2022-11-26 21:13:08 +0530

    added new credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 const firebaseConfig = {
-    apiKey: "AIzaSyCggZCcBun0cwNfOWGC2K8pZcgIRWMfqwY",
-    authDomain: "olx-sijeesh.firebaseapp.com",
-    projectId: "olx-sijeesh",
-    storageBucket: "olx-sijeesh.appspot.com",
-    messagingSenderId: "767411886432",
-    appId: "1:767411886432:web:2ef6862afc88f2c423a605",
-    measurementId: "G-4ELNR9DJHL"
-  };
+  apiKey: "AIzaSyCZ_ynzcVnIrNymT_EoCCXf2oy2O6P4AvA",
+  authDomain: "olx-clonee-a855d.firebaseapp.com",
+  projectId: "olx-clonee-a855d",
+  storageBucket: "olx-clonee-a855d.appspot.com",
+  messagingSenderId: "767340040920",
+  appId: "1:767340040920:web:94bd069cae46a0cea17dad",
+  measurementId: "G-ZMB6Q9LESN"
+};
 
   export const Firebase= firebase.initializeApp(firebaseConfig)//named export
